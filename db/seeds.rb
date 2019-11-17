bcc7641653966f089facd9baaa33777afff7605f
HTanigawa-10 <tanigawa@aaaa.com>
2019-11-18 00:18:57 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,58 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
+
+# coding: utf-8
+
+Article.create(
+  AUTHER: 'tani',
+  TITLE: 'VSCode リモートファイル編集',
+  SUMMERY: 'VSCodeのプラグイン「SSH FS」を使用したリモートファイル編集の方法について書きます。XXXX～XXXXま で。',
+  LINK: '.html',
+  IMAGE_PATH: 'top/vscode.png'
+)
+Article.create(
+  AUTHER: 'tani',
+  TITLE: 'Ruby on Rails環境構築（Linux）①',
+  SUMMERY: 'Ruby on Railsの構築手順を書きます。<br>XXXX～XXXXまで<br>',
+  LINK: '.html',
+  IMAGE_PATH: 'top/rails.png'
+)
+Article.create(
+  AUTHER: 'tani',
+  TITLE: 'Ruby on Rails環境構築（Linux）②',
+  SUMMERY: 'Ruby on Railsの構築手順を書きます。<br>XXXX～XXXXまで<br>',
+  LINK: '.html',
+  IMAGE_PATH: 'top/rails.png'
+)
+Article.create(
+  AUTHER: 'tani',
+  TITLE: 'Ruby on Rails環境構築（Linux）③',
+  SUMMERY: 'Ruby on Railsの構築手順を書きます。<br>XXXX～XXXXまで<br>',
+  LINK: '.html',
+  IMAGE_PATH: 'top/rails.png'
+)
+Article.create(
+  AUTHER: 'tani',
+  TITLE: '正規表現',
+  SUMMERY: '正規表現について書きます。',
+  LINK: '.html',
+  IMAGE_PATH: 'top/seikihyougen.jpg'
+)
+Article.create(
+  AUTHER: 'tani',
+  TITLE: 'Docker',
+  SUMMERY: 'Dockerについて書きます。',
+  LINK: '.html',
+  IMAGE_PATH: 'top/docker.png'
+)
+Article.create(
+  AUTHER: 'tani',
+  TITLE: 'Unity 3D制作',
+  SUMMERY: 'Unityを用いた3Dゲーム制作について書きます。',
+  LINK: 'unity.html',
+  IMAGE_PATH: 'top/unity.jpg'
