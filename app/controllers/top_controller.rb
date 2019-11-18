0dd14ed25d1f7a521ced952564f17f67ad768b6d
HTanigawa-10 <tanigawa@aaaa.com>
2019-11-18 22:24:56 +0900

    不足分追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,23 @@
 #coding: utf-8
 class TopController < ApplicationController
-    def index
-      gon.articles = Article.all
-      gon.count = Article.count
-    end
+  def index
+    gon.articles = Article.all
+    gon.count = Article.count
+    #@article_dom = [];
+
+    #for article in Article.all do
+    #  dom = '<div class="photoText">最終更新:<%=' +
+    #  article.updated_at.strftime('%Y/%m/%d') +
+    #  '%><%= image_tag(\"\"' +
+    #  #article.IMAGE_PATH +
+    #  ') %><h4><%=' +
+    #  article.TITLE +
+    #  '%></h4><p><%=' +
+    #  article.SUMMERY +
+    #  '%></p></div><!-- /.photoText -->';
+#
+    #  @article_dom.push(dom)
+    #end
+    #gon.article_dom = @article_dom
+  end
 end
